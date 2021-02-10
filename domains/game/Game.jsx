ffc79ae7a4a77e9163e2cff491af3fbfdcd7e958
Guillaume <guillaume.billey@gmail.com>
2021-02-10 14:15:55 +0100

    fix(current): apply review advices

--- domains/game/Game.jsx
+++ domains/game/Game.jsx
@@ -3,25 +3,19 @@ import { Text, View } from "react-native";
 import PropTypes from "prop-types";
 
 const Game = ({ playerName, game }) => {
-    const renderCurrentPlayer = (playerName) => {
-        return <Text key={playerName}>{playerName} is playing</Text>;
-    };
-
-    const renderWaitingPlayer = (playerName) => {
-        return <Text key={playerName}>{playerName} is waiting</Text>;
-    };
-
     return (
         <View>
             <Text>
                 Game {game.id}, you are {playerName}
             </Text>
             {game.players.map((player) => {
-                if (player.current) {
-                    return renderCurrentPlayer(player.name);
-                } else {
-                    return renderWaitingPlayer(player.name);
-                }
+                return (
+                    <Text key={playerName}>
+                        {`${playerName} is ${
+                            player.current ? "playing" : "waiting"
+                        }`}
+                    </Text>
+                );
             })}
         </View>
     );
