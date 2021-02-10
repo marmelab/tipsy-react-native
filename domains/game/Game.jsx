d481299f44bcfe571570a455a0df4cbd09bfaee8
Guillaume <guillaume.billey@gmail.com>
2021-02-10 14:02:06 +0100

    fix(currentPlayer): fix currentPlayer display

--- domains/game/Game.jsx
+++ domains/game/Game.jsx
@@ -3,18 +3,25 @@ import { Text, View } from "react-native";
 import PropTypes from "prop-types";
 
 const Game = ({ playerName, game }) => {
+    const renderCurrentPlayer = (playerName) => {
+        return <Text key={playerName}>{playerName} is playing</Text>;
+    };
+
+    const renderWaitingPlayer = (playerName) => {
+        return <Text key={playerName}>{playerName} is waiting</Text>;
+    };
+
     return (
         <View>
-            <Text>New Game</Text>
+            <Text>
+                Game {game.id}, you are {playerName}
+            </Text>
             {game.players.map((player) => {
-                if (playerName === player.name) {
-                    return (
-                        <Text key={player.name}>
-                            {player.name} You are playing
-                        </Text>
-                    );
+                if (player.current) {
+                    return renderCurrentPlayer(player.name);
+                } else {
+                    return renderWaitingPlayer(player.name);
                 }
-                return <Text key={player.name}>{player.name} Is waiting</Text>;
             })}
         </View>
     );
